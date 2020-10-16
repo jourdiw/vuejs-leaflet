adbe2529266ba918b04839eb8f5aca35a7b0df8b
jourdiw <jourdiwaller@gmail.com>
2020-10-16 23:29:08 +0200

    Addition of Auth0 to the front and a quick syntax fix to the backend

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,11 +1,80 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+<div>
+  <div>
+      <button
+        class="btn btn-primary btn-margin"
+        v-if="!authenticated"
+        @click="login()">
+        Log In
+      </button>
+
+      <button
+        class="btn btn-primary btn-margin"
+        v-if="authenticated"
+        @click="privateMessage()">
+        Call Private
+      </button>
+
+      <button
+        class="btn btn-primary btn-margin"
+        v-if="authenticated"
+        @click="logout()">
+        Log Out
+      </button>
+      {{ message }}
+      <br>
     </div>
-    <router-view />
-  </div>
+    <div id="app">
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+      <router-view />
+    </div>
+</div>
 </template>
-<script></script>
+<script>
+import AuthService from './auth/AuthService'
+import axios from 'axios'
+
+const API_URL = 'http://localhost:8000'
+const auth = new AuthService()
+
+export default {
+  name: 'app',
+  data () {
+    this.handleAuthentication()
+    this.authenticated = false
+
+    auth.authNotifier.on('authChange', authState => {
+      this.authenticated = authState.authenticated
+    })
+
+    return {
+      authenticated: false,
+      message: ''
+    }
+  },
+
+  methods: {
+    // this method calls the AuthService login() method
+    login () {
+      auth.login()
+    },
+    handleAuthentication () {
+      auth.handleAuthentication()
+    },
+    logout () {
+      auth.logout()
+    },
+    privateMessage () {
+      const url = `${API_URL}/api/private/`
+      return axios.get(url, {headers: {Authorization: `Bearer ${auth.getAuthToken()}`}}).then((response) => {
+        console.log(response.data)
+        this.message = response.data || ''
+      })
+    }
+  },
+}
+</script>
 <style lang="stylus"></style>
